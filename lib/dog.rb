615771c6037e2bfd966977679d32026a07100dc3
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-08-23 00:24:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,10 @@ class Dog
   
   def self.create_table
     sql = <<-SQL
-    CREATE THE TABLE IF NOT EXISTS dogs ()
+    CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT)
   end
   
 end
