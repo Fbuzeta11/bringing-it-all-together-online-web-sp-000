d0fa1f23e136657aa3e2c6fff946e4fdee1c4e81
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-08-23 00:38:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,9 @@ class Dog
     SQL
     
     DB[:conn].execute(sql, self.name, self.breed)
-    
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+    self
   end
   
 end
